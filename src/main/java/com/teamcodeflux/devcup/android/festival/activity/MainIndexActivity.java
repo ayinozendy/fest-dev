de8cfe551f15a096876277a6e98106fd85fdef56
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-08-17 12:46:48 +0800

    Initial commit with generated Android and Maven files
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

---
+++ src/main/java/com/teamcodeflux/devcup/android/festival/activity/MainIndexActivity.java
@@ -0,0 +1,16 @@
+package com.teamcodeflux.devcup.android.festival.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.teamcodeflux.devcup.android.festival.R;
+
+public class MainIndexActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
