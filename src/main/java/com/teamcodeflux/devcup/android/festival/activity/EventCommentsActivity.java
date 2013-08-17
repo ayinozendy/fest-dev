578e696afb28865ee72107880332e387fe8a9254
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-08-17 15:43:44 +0800

    Add comments list adapter for list of posts of event
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

--- src/main/java/com/teamcodeflux/devcup/android/festival/activity/EventCommentsActivity.java
+++ src/main/java/com/teamcodeflux/devcup/android/festival/activity/EventCommentsActivity.java
@@ -1,10 +1,58 @@
 package com.teamcodeflux.devcup.android.festival.activity;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
 import com.actionbarsherlock.app.SherlockActivity;
 import com.googlecode.androidannotations.annotations.EActivity;
 import com.teamcodeflux.devcup.android.festival.R;
+import com.teamcodeflux.devcup.android.festival.model.Post;
+
+import java.util.List;
 
 @EActivity(R.layout.event_comments_layout)
 public class EventCommentsActivity extends SherlockActivity {
 
+    private class CommentsListAdapter extends BaseAdapter {
+
+        private LayoutInflater layoutInflater;
+
+        private List<Post> listOfPosts;
+
+        public CommentsListAdapter(Context context, List<Post> posts) {
+            layoutInflater = LayoutInflater.from(context);
+            listOfPosts = posts;
+        }
+
+        @Override
+        public int getCount() {
+            return listOfPosts.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return listOfPosts.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = this.layoutInflater.inflate(R.layout.event_comments_item_layout, parent, false);
+
+            TextView nameField = (TextView) view.findViewById(R.id.name);
+            nameField.setText(listOfPosts.get(position).getName());
+
+            TextView postBodyField = (TextView) view.findViewById(R.id.post_body);
+            postBodyField.setText(listOfPosts.get(position).getPostBody());
+
+            return view;
+        }
+    }
 }
