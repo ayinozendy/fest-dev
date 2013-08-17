cc2c38fb504cfe5741167a10268dcc04742c6150
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-08-17 23:38:51 +0800

    Create activity and layout for event details map view
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

---
+++ src/main/java/com/teamcodeflux/devcup/android/festival/activity/EventMapViewActivity.java
@@ -0,0 +1,10 @@
+package com.teamcodeflux.devcup.android.festival.activity;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.googlecode.androidannotations.annotations.EActivity;
+import com.teamcodeflux.devcup.android.festival.R;
+
+@EActivity(R.layout.event_details_map_view_layout)
+public class EventMapViewActivity extends SherlockFragmentActivity {
+
+}
