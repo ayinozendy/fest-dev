0153073d55e19ee2898f9b20f207b3564ba20e77
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-08-17 15:33:02 +0800

    Create Event comments page Activity and Layout
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

---
+++ src/main/java/com/teamcodeflux/devcup/android/festival/activity/EventCommentsActivity.java
@@ -0,0 +1,10 @@
+package com.teamcodeflux.devcup.android.festival.activity;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.googlecode.androidannotations.annotations.EActivity;
+import com.teamcodeflux.devcup.android.festival.R;
+
+@EActivity(R.layout.event_comments_layout)
+public class EventCommentsActivity extends SherlockActivity {
+
+}
