9b87761162526564cc72eef547dabe3952d3b088
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-08-18 00:32:58 +0800

    Implement posting comment functionality
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

--- src/main/java/com/teamcodeflux/devcup/android/festival/activity/EventCommentPostingActivity.java
+++ src/main/java/com/teamcodeflux/devcup/android/festival/activity/EventCommentPostingActivity.java
@@ -1,14 +1,41 @@
 package com.teamcodeflux.devcup.android.festival.activity;
 
+import android.widget.EditText;
 import com.actionbarsherlock.app.SherlockActivity;
-import com.googlecode.androidannotations.annotations.EActivity;
-import com.googlecode.androidannotations.annotations.Extra;
+import com.googlecode.androidannotations.annotations.*;
 import com.teamcodeflux.devcup.android.festival.R;
 import com.teamcodeflux.devcup.android.festival.model.Event;
+import com.teamcodeflux.devcup.android.festival.model.Post;
+import com.teamcodeflux.devcup.android.festival.service.RestMethod;
+import org.apache.commons.lang3.StringUtils;
 
 @EActivity(R.layout.event_comment_posting_layout)
 public class EventCommentPostingActivity extends SherlockActivity {
 
     @Extra
     Event event;
+
+    @ViewById(R.id.username)
+    EditText usernameField;
+
+    @ViewById(R.id.comment)
+    EditText commentField;
+
+    @Click(R.id.post)
+    void post() {
+        validateFields();
+        postComment();
+    }
+
+    private void validateFields() {
+        if (StringUtils.isBlank(usernameField.getText()) || StringUtils.isBlank(commentField.getText())) {
+            //Alert!
+        }
+    }
+
+    @Background
+    void postComment() {
+        Post post = Post.buildPost(0, usernameField.getText().toString(), "", commentField.getText().toString(), event.getId());
+        RestMethod.postComment(post);
+    }
 }
