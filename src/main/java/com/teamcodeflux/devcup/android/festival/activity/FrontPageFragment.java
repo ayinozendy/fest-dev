300caf1bf5a5c20896a66d6304f878b70b7ce998
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-08-17 14:02:22 +0800

    Create ListView adapter for Post
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

--- src/main/java/com/teamcodeflux/devcup/android/festival/activity/FrontPageFragment.java
+++ src/main/java/com/teamcodeflux/devcup/android/festival/activity/FrontPageFragment.java
@@ -7,4 +7,43 @@ import com.teamcodeflux.devcup.android.festival.R;
 @EFragment(R.layout.front_page_layout)
 public class FrontPageFragment extends SherlockFragment {
 
+
+    private class PostListAdapter extends BaseAdapter {
+
+        private LayoutInflater layoutInflater;
+
+        private List<Post> listOfPosts;
+
+        public PostListAdapter(List<Post> posts) {
+            listOfPosts = posts;
+        }
+
+        @Override
+        public int getCount() {
+            return listOfPosts.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return listOfPosts.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = this.layoutInflater.inflate(R.layout.front_page_item_layout, parent, false);
+
+            TextView nameField = (TextView) view.findViewById(R.id.name);
+            nameField.setText(listOfPosts.get(position).getName());
+
+            TextView postBodyField = (TextView) view.findViewById(R.id.post_body);
+            postBodyField.setText(listOfPosts.get(position).getName());
+
+            return view;
+        }
+    }
 }
