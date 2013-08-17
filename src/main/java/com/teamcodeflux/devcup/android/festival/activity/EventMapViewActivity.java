2363a42384ee6ea47adfb9d6a511bad90402dc15
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-08-17 23:53:03 +0800

    Set coordinates from extra
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

--- src/main/java/com/teamcodeflux/devcup/android/festival/activity/EventMapViewActivity.java
+++ src/main/java/com/teamcodeflux/devcup/android/festival/activity/EventMapViewActivity.java
@@ -4,12 +4,18 @@ import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 import com.googlecode.androidannotations.annotations.AfterViews;
 import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.Extra;
 import com.teamcodeflux.devcup.android.festival.R;
+import com.teamcodeflux.devcup.android.festival.model.Event;
 
 @EActivity(R.layout.event_details_map_view_layout)
 public class EventMapViewActivity extends SherlockFragmentActivity {
+
+    @Extra
+    Event event;
 
     private static final float ZOOM_LEVEL = 16f;
 
@@ -24,10 +30,6 @@ public class EventMapViewActivity extends SherlockFragmentActivity {
         googleMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
         googleMap.setMyLocationEnabled(true);
         googleMap.moveCamera(CameraUpdateFactory.zoomTo(ZOOM_LEVEL));
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(event.getLatitude(), event.getLongitude())));
     }
 }
