0aee7fb3e9fb96bd3b7528755cbd3ba91c3d00eb
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-08-17 15:00:05 +0800

    Create Event detail page Activity and Layout
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

---
+++ src/main/java/com/teamcodeflux/devcup/android/festival/activity/EventDetailsPageActivity.java
@@ -0,0 +1,10 @@
+package com.teamcodeflux.devcup.android.festival.activity;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.googlecode.androidannotations.annotations.EActivity;
+import com.teamcodeflux.devcup.android.festival.R;
+
+@EActivity(R.layout.event_details_layout)
+public class EventDetailsPageActivity extends SherlockActivity {
+
+}
